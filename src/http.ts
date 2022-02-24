342d5dffcd4853ca852d7d472035e27fac63b037
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-24 13:23:48 -0300

    First commit

---
+++ src/http.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { createServer } from "http";
+import { routes } from "./routes";
+
+const app = express();
+const http = createServer(app);
+
+app.use(express.json());
+app.use(routes);
+
+export {http, app};
