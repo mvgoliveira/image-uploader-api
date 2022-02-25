aeed0004ab26c3ed400d89162ee233aac4df4a65
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-25 14:33:17 -0300

    added environment variables and Cors

--- src/http.ts
+++ src/http.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { routes } from "./routes";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 const http = createServer(app);
@@ -9,6 +10,7 @@ const http = createServer(app);
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
